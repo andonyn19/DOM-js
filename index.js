832cddc239e725debd2c179982f8cc1b9795b5f4
andonyn19 <andonynunez2@gmail.com>
2022-05-24 19:04:17 -0500

    Setup cuentaRegresiva

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const d = document,
 
 import menuHamburguesa from './module/menuHamburguesa.js';
 import { relojDigital, alarma } from './module/relojAlarma.js';
+import cuentaRegresiva from './module/cuentaRegresiva.js';
 
 d.addEventListener("DOMContentLoaded",e =>{
   menuHamburguesa(".hamburger", ".panel", ".menu");
   relojDigital("activar-reloj", "desactivar-reloj", "reloj");
-  alarma("activar-alarma", "desactivar-alarma", "./alarm.mp3")
+  alarma("activar-alarma", "desactivar-alarma", "./alarm.mp3");
+  cuentaRegresiva("countdown", "2023, 03, 11")
 })
